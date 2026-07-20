bb84db9b5ec1a9bc80cdd9ec7aac51e0f1b97990
Sree Varsha <msreevarsha05@gmail.com>
2026-07-20 21:29:56 +0530

    SendToken.js change

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -9,6 +9,8 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_EXPIRES_TIME * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: true,      // MUST be true for Render/Vercel
+    sameSite: "none",  // MUST be "none" for cross-site cookies
   };
 
   res.cookie("jwt", token, cookieOptions);
